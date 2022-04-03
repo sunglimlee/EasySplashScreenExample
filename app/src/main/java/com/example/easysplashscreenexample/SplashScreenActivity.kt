7a0f2f5d61407115d9033d90e64ed13c94938439
Steve Lee <steve.patriot@gmail.com>
2022-04-03 08:19:15 -0400

    Initial commit EasySplashScreen 이상하게도 객체가 보이지를 않아서 쓸수가 없네..

---
+++ app/src/main/java/com/example/easysplashscreenexample/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.easysplashscreenexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Activity를 새로 생성해서 EasySplashScreen을 사용해야 하는데 객체가 안보인다.
+    }
+}
